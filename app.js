31bb6b4219c80d8bbc7f1e16859341b0b50c06e3
bion <bionjohnson@gmail.com>
2017-03-04 22:23:38 -0600

    Add game data

---
+++ app.js
@@ -0,0 +1,208 @@
+window.NSG = {};
+
+NSG.GAME_DATA = [
+  {
+    "date": "Saturday, March 4, 2017 5:30PM PT",
+    "matchup": "at HOUSTON DYNAMO",
+    "location": "HOUSTON, TX",
+    "broadcast": "Broadcast: Q13 FOX, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, March 11, 2017 4:00PM PT",
+    "matchup": "at MONTREAL IMPACT",
+    "location": "MONTREAL, QC",
+    "broadcast": "Broadcast: JOEtv, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Sunday, March 19, 2017 4:00PM PT",
+    "matchup": "NEW YORK RED BULLS",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: FS1, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Friday, March 31, 2017 7:00PM PT",
+    "matchup": "ATLANTA UNITED FC",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: FS1, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, April 8, 2017 7:30PM PT",
+    "matchup": "at SAN JOSE EARTHQUAKES",
+    "location": "SAN JOSE, CA",
+    "broadcast": "Broadcast: Q13 FOX, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Friday, April 14, 2017 7:00PM PT",
+    "matchup": "at VANCOUVER WHITECAPS FC",
+    "location": "VANCOUVER, BC",
+    "broadcast": "Broadcast: Q13 FOX, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Sunday, April 23, 2017 1:00PM PT",
+    "matchup": "at LA GALAXY",
+    "location": "CARSON, CA",
+    "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, April 29, 2017 7:00PM PT",
+    "matchup": "NEW ENGLAND REVOLUTION",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: JOEtv, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, May 6, 2017 1:00PM PT",
+    "matchup": "TORONTO FC",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, May 13, 2017 5:30PM PT",
+    "matchup": "at CHICAGO FIRE",
+    "location": "BRIDGEVIEW, IL",
+    "broadcast": "Broadcast: Q13 FOX, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Wednesday, May 17, 2017 5:30PM PT",
+    "matchup": "at SPORTING KANSAS CITY",
+    "location": "KANSAS CITY, KS",
+    "broadcast": "Broadcast: JOEtv, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, May 20, 2017 2:00PM PT",
+    "matchup": "REAL SALT LAKE",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: JOEtv, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, May 27, 2017 12:00PM PT",
+    "matchup": "PORTLAND TIMBERS",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: FOX, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Wednesday, May 31, 2017 4:30PM PT",
+    "matchup": "at COLUMBUS CREW SC",
+    "location": "COLUMBUS, OH",
+    "broadcast": "Broadcast: Q13 FOX, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Sunday, June 4, 2017 7:00PM PT",
+    "matchup": "HOUSTON DYNAMO",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: JOEtv, Univision-Seattle, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, June 17, 2017 10:00AM PT",
+    "matchup": "at NEW YORK CITY FC",
+    "location": "BRONX, NY",
+    "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Wednesday, June 21, 2017 7:30PM PT",
+    "matchup": "ORLANDO CITY",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: JOEtv, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Sunday, June 25, 2017 1:00PM PT",
+    "matchup": "at PORTLAND TIMBERS",
+    "location": "PORTLAND, OR",
+    "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Tuesday, July 4, 2017 6:00PM PT",
+    "matchup": "at COLORADO RAPIDS",
+    "location": "COMMERCE CITY, CO",
+    "broadcast": "Broadcast: Q13 FOX, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Wednesday, July 19, 2017 7:30PM PT",
+    "matchup": "D.C. UNITED",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: Q13 FOX, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Sunday, July 23, 2017 7:00PM PT",
+    "matchup": "SAN JOSE EARTHQUAKES",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: JOEtv, Univision-Seattle, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, July 29, 2017 TBD",
+    "matchup": "at LA GALAXY",
+    "location": "CARSON, CA",
+    "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, August 5, 2017 5:00PM PT",
+    "matchup": "at MINNESOTA UNITED FC",
+    "location": "MINNEAPOLIS, MN",
+    "broadcast": "Broadcast: Q13 FOX, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, August 12, 2017 1:00PM PT",
+    "matchup": "SPORTING KANSAS CITY",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: JOEtv, Univision-Seattle, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Sunday, August 20, 2017 7:00PM PT",
+    "matchup": "MINNESOTA UNITED FC",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: FS1, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Wednesday, August 23, 2017 7:00PM PT",
+    "matchup": "at VANCOUVER WHITECAPS FC",
+    "location": "VANCOUVER, BC",
+    "broadcast": "Broadcast: JOEtv, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Sunday, August 27, 2017 6:30PM PT",
+    "matchup": "PORTLAND TIMBERS",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: FS1, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Sunday, September 10, 2017 6:00PM PT",
+    "matchup": "LA GALAXY",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: FS1, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, September 16, 2017 5:00PM PT",
+    "matchup": "at FC DALLAS",
+    "location": "FRISCO, TX",
+    "broadcast": "Broadcast: JOEtv, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Saturday, September 23, 2017 6:30PM PT",
+    "matchup": "at REAL SALT LAKE",
+    "location": "SANDY, UT",
+    "broadcast": "Broadcast: JOEtv, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Wednesday, September 27, 2017 7:30PM PT",
+    "matchup": "VANCOUVER WHITECAPS FC",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: JOEtv, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Sunday, October 1, 2017 10:00AM PT",
+    "matchup": "at PHILADELPHIA UNION",
+    "location": "CHESTER, PA",
+    "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Sunday, October 15, 2017 2:00PM PT",
+    "matchup": "FC DALLAS",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: JOEtv, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM"
+  },
+  {
+    "date": "Sunday, October 22, 2017 1:00PM PT",
+    "matchup": "COLORADO RAPIDS",
+    "location": "SEATTLE, WA",
+    "broadcast": "Broadcast: JOEtv, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM"
+  }
+];
