cfc563f1858309d6904486ff2025e4c9e4f10d12
bion <bionjohnson@gmail.com>
2017-03-06 18:08:04 -0800

    Version -1.0

--- app.js
+++ app.js
@@ -2,238 +2,238 @@ window.NSG = {};
 
 NSG.GAME_DATA = [
   {
-    "date": "Saturday, March 4, 2017 5:30PM PT",
-    "matchup": "at HOUSTON DYNAMO",
+    "date": "Saturday, March 4, 5:30PM PST",
+    "matchup": "HOUSTON DYNAMO",
     "location": "HOUSTON, TX",
     "broadcast": "Broadcast: Q13 FOX, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1488673800000
   },
   {
-    "date": "Saturday, March 11, 2017 4:00PM PT",
-    "matchup": "at MONTREAL IMPACT",
+    "date": "Saturday, March 11, 4:00PM PST",
+    "matchup": "MONTREAL IMPACT",
     "location": "MONTREAL, QC",
     "broadcast": "Broadcast: JOEtv, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1489276800000
   },
   {
-    "date": "Sunday, March 19, 2017 4:00PM PT",
+    "date": "Sunday, March 19, 4:00PM PST",
     "matchup": "NEW YORK RED BULLS",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: FS1, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1489964400000
   },
   {
-    "date": "Friday, March 31, 2017 7:00PM PT",
+    "date": "Friday, March 31, 7:00PM PST",
     "matchup": "ATLANTA UNITED FC",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: FS1, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1491012000000
   },
   {
-    "date": "Saturday, April 8, 2017 7:30PM PT",
-    "matchup": "at SAN JOSE EARTHQUAKES",
+    "date": "Saturday, April 8, 7:30PM PST",
+    "matchup": "SAN JOSE EARTHQUAKES",
     "location": "SAN JOSE, CA",
     "broadcast": "Broadcast: Q13 FOX, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1491705000000
   },
   {
-    "date": "Friday, April 14, 2017 7:00PM PT",
-    "matchup": "at VANCOUVER WHITECAPS FC",
+    "date": "Friday, April 14, 7:00PM PST",
+    "matchup": "VANCOUVER WHITECAPS FC",
     "location": "VANCOUVER, BC",
     "broadcast": "Broadcast: Q13 FOX, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1492221600000
   },
   {
-    "date": "Sunday, April 23, 2017 1:00PM PT",
-    "matchup": "at LA GALAXY",
+    "date": "Sunday, April 23, 1:00PM PST",
+    "matchup": "LA GALAXY",
     "location": "CARSON, CA",
     "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1492977600000
   },
   {
-    "date": "Saturday, April 29, 2017 7:00PM PT",
+    "date": "Saturday, April 29, 7:00PM PST",
     "matchup": "NEW ENGLAND REVOLUTION",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: JOEtv, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1493517600000
   },
   {
-    "date": "Saturday, May 6, 2017 1:00PM PT",
+    "date": "Saturday, May 6, 1:00PM PST",
     "matchup": "TORONTO FC",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1494100800000
   },
   {
-    "date": "Saturday, May 13, 2017 5:30PM PT",
-    "matchup": "at CHICAGO FIRE",
+    "date": "Saturday, May 13, 5:30PM PST",
+    "matchup": "CHICAGO FIRE",
     "location": "BRIDGEVIEW, IL",
     "broadcast": "Broadcast: Q13 FOX, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1494721800000
   },
   {
-    "date": "Wednesday, May 17, 2017 5:30PM PT",
-    "matchup": "at SPORTING KANSAS CITY",
+    "date": "Wednesday, May 17, 5:30PM PST",
+    "matchup": "SPORTING KANSAS CITY",
     "location": "KANSAS CITY, KS",
     "broadcast": "Broadcast: JOEtv, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1495067400000
   },
   {
-    "date": "Saturday, May 20, 2017 2:00PM PT",
+    "date": "Saturday, May 20, 2:00PM PST",
     "matchup": "REAL SALT LAKE",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: JOEtv, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1495314000000
   },
   {
-    "date": "Saturday, May 27, 2017 12:00PM PT",
+    "date": "Saturday, May 27, 12:00PM PST",
     "matchup": "PORTLAND TIMBERS",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: FOX, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1495911600000
   },
   {
-    "date": "Wednesday, May 31, 2017 4:30PM PT",
-    "matchup": "at COLUMBUS CREW SC",
+    "date": "Wednesday, May 31, 4:30PM PST",
+    "matchup": "COLUMBUS CREW SC",
     "location": "COLUMBUS, OH",
     "broadcast": "Broadcast: Q13 FOX, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1496273400000
   },
   {
-    "date": "Sunday, June 4, 2017 7:00PM PT",
+    "date": "Sunday, June 4, 7:00PM PST",
     "matchup": "HOUSTON DYNAMO",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: JOEtv, Univision-Seattle, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1496628000000
   },
   {
-    "date": "Saturday, June 17, 2017 10:00AM PT",
-    "matchup": "at NEW YORK CITY FC",
+    "date": "Saturday, June 17, 10:00AM PST",
+    "matchup": "NEW YORK CITY FC",
     "location": "BRONX, NY",
     "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1497718800000
   },
   {
-    "date": "Wednesday, June 21, 2017 7:30PM PT",
+    "date": "Wednesday, June 21, 7:30PM PST",
     "matchup": "ORLANDO CITY",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: JOEtv, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1498098600000
   },
   {
-    "date": "Sunday, June 25, 2017 1:00PM PT",
-    "matchup": "at PORTLAND TIMBERS",
+    "date": "Sunday, June 25, 1:00PM PST",
+    "matchup": "PORTLAND TIMBERS",
     "location": "PORTLAND, OR",
     "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1498420800000
   },
   {
-    "date": "Tuesday, July 4, 2017 6:00PM PT",
-    "matchup": "at COLORADO RAPIDS",
+    "date": "Tuesday, July 4, 6:00PM PST",
+    "matchup": "COLORADO RAPIDS",
     "location": "COMMERCE CITY, CO",
     "broadcast": "Broadcast: Q13 FOX, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1499216400000
   },
   {
-    "date": "Wednesday, July 19, 2017 7:30PM PT",
+    "date": "Wednesday, July 19, 7:30PM PST",
     "matchup": "D.C. UNITED",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: Q13 FOX, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1500517800000
   },
   {
-    "date": "Sunday, July 23, 2017 7:00PM PT",
+    "date": "Sunday, July 23, 7:00PM PST",
     "matchup": "SAN JOSE EARTHQUAKES",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: JOEtv, Univision-Seattle, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1500861600000
   },
   {
-    "date": "Saturday, July 29, 2017 TBD",
-    "matchup": "at LA GALAXY",
+    "date": "Saturday, July 29, TBD",
+    "matchup": "LA GALAXY",
     "location": "CARSON, CA",
     "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1501311600000
   },
   {
-    "date": "Saturday, August 5, 2017 5:00PM PT",
-    "matchup": "at MINNESOTA UNITED FC",
+    "date": "Saturday, August 5, 5:00PM PST",
+    "matchup": "MINNESOTA UNITED FC",
     "location": "MINNEAPOLIS, MN",
     "broadcast": "Broadcast: Q13 FOX, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1501977600000
   },
   {
-    "date": "Saturday, August 12, 2017 1:00PM PT",
+    "date": "Saturday, August 12, 1:00PM PST",
     "matchup": "SPORTING KANSAS CITY",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: JOEtv, Univision-Seattle, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1502568000000
   },
   {
-    "date": "Sunday, August 20, 2017 7:00PM PT",
+    "date": "Sunday, August 20, 7:00PM PST",
     "matchup": "MINNESOTA UNITED FC",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: FS1, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1503280800000
   },
   {
-    "date": "Wednesday, August 23, 2017 7:00PM PT",
-    "matchup": "at VANCOUVER WHITECAPS FC",
+    "date": "Wednesday, August 23, 7:00PM PST",
+    "matchup": "VANCOUVER WHITECAPS FC",
     "location": "VANCOUVER, BC",
     "broadcast": "Broadcast: JOEtv, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1503540000000
   },
   {
-    "date": "Sunday, August 27, 2017 6:30PM PT",
+    "date": "Sunday, August 27, 6:30PM PST",
     "matchup": "PORTLAND TIMBERS",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: FS1, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1503883800000
   },
   {
-    "date": "Sunday, September 10, 2017 6:00PM PT",
+    "date": "Sunday, September 10, 6:00PM PST",
     "matchup": "LA GALAXY",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: FS1, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1505091600000
   },
   {
-    "date": "Saturday, September 16, 2017 5:00PM PT",
-    "matchup": "at FC DALLAS",
+    "date": "Saturday, September 16, 5:00PM PST",
+    "matchup": "FC DALLAS",
     "location": "FRISCO, TX",
     "broadcast": "Broadcast: JOEtv, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1505606400000
   },
   {
-    "date": "Saturday, September 23, 2017 6:30PM PT",
-    "matchup": "at REAL SALT LAKE",
+    "date": "Saturday, September 23, 6:30PM PST",
+    "matchup": "REAL SALT LAKE",
     "location": "SANDY, UT",
     "broadcast": "Broadcast: JOEtv, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1506216600000
   },
   {
-    "date": "Wednesday, September 27, 2017 7:30PM PT",
+    "date": "Wednesday, September 27, 7:30PM PST",
     "matchup": "VANCOUVER WHITECAPS FC",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: JOEtv, ROOT Sports (Outside Seattle), KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1506565800000
   },
   {
-    "date": "Sunday, October 1, 2017 10:00AM PT",
-    "matchup": "at PHILADELPHIA UNION",
+    "date": "Sunday, October 1, 10:00AM PST",
+    "matchup": "PHILADELPHIA UNION",
     "location": "CHESTER, PA",
     "broadcast": "Broadcast: ESPN, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1506877200000
   },
   {
-    "date": "Sunday, October 15, 2017 2:00PM PT",
+    "date": "Sunday, October 15, 2:00PM PST",
     "matchup": "FC DALLAS",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: JOEtv, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM",
     "time_in_milliseconds": 1508101200000
   },
   {
-    "date": "Sunday, October 22, 2017 1:00PM PT",
+    "date": "Sunday, October 22, 1:00PM PST",
     "matchup": "COLORADO RAPIDS",
     "location": "SEATTLE, WA",
     "broadcast": "Broadcast: JOEtv, Univision-Seattle, KIRO Radio 97.3 FM, El Rey 1360AM",
@@ -244,8 +244,8 @@ NSG.GAME_DATA = [
 window.onload = onPageLoad;
 
 function onPageLoad() {
-  renderClock();
-  setInterval(renderClock, 1000);
+  renderGame();
+  setInterval(renderGame, 1000);
 }
 
 NSG.MILLIS_PER_DAY    = 86400000;
@@ -259,24 +259,65 @@ function formatTimeDifference(timeDiffMillis) {
   var minutes = Math.floor((timeDiffMillis - ((hours * NSG.MILLIS_PER_HOUR) + (days * NSG.MILLIS_PER_DAY))) / NSG.MILLIS_PER_MINUTE);
   var seconds = Math.floor((timeDiffMillis - ((hours * NSG.MILLIS_PER_HOUR) + (days * NSG.MILLIS_PER_DAY) + (minutes * NSG.MILLIS_PER_MINUTE))) / NSG.MILLIS_PER_SECOND);
 
-  return [days, hours, minutes, seconds].join(':');
+  return [
+    days + ' days',
+    hours + ' hours',
+    minutes + ' minutes',
+    seconds + ' seconds'
+  ].join('<br>');
 }
 
-function getNextGame(now) {
+function getGame(pred) {
+  var game;
+
   for (var i = 0; i < NSG.GAME_DATA.length; i++) {
-    if (NSG.GAME_DATA[i].time_in_milliseconds < now) {
-      return NSG.GAME_DATA[i + 1];
+    game = NSG.GAME_DATA[i];
+
+    if (pred(game)) {
+      return game
     }
   }
-
-  return null;
 }
 
-function renderClock() {
+function futureGamePred() {
+  var now = new Date().getTime();
+
+  return function(game) {
+    return game.time_in_milliseconds > now;
+  }
+}
+
+function isHomeGame(game) {
+  return game.location === 'SEATTLE, WA';
+}
+
+function futureHomeGamePred() {
+  var isFutureGame = futureGamePred();
+
+  return function(game) {
+    return isFutureGame(game) && isHomeGame(game);
+  }
+}
+
+function renderGame() {
   var countdown = document.getElementsByClassName('countdown')[0];
   var nowMillis = new Date().getTime();
-  var nextGame = getNextGame(nowMillis);
+  var nextGame = getGame(futureGamePred());
   var timeDiffMillis = nextGame.time_in_milliseconds - nowMillis;
+  var homeAway = nextGame.location === "SEATTLE, WA" ? 'Home' : 'Away';
 
-  countdown.textContent = formatTimeDifference(timeDiffMillis);
+  countdown.innerHTML = formatTimeDifference(timeDiffMillis);
+
+  document.getElementsByClassName('game-content')[0].innerHTML = nextGame.date + '<br>' + [homeAway, 'vs', nextGame.matchup].join(' ');
+
+  if (homeAway === 'Away') {
+    displayNextHomeGame(nowMillis);
+  }
+}
+
+function displayNextHomeGame(now) {
+  var nextHomeGame = getGame(futureHomeGamePred());
+
+  document.getElementsByClassName('home-game-header')[0].textContent = 'Next home game';
+  document.getElementsByClassName('home-game-content')[0].innerHTML = nextHomeGame.date + '<br>' + 'vs ' + nextHomeGame.matchup;
 }
--- index.rb
+++ index.rb
@@ -17,7 +17,38 @@ class Index < Fortitude::Widget
 
   def page_body
     body do
-      h1(class: 'countdown')
+      # h1('sounders', class: 'sounders')
+
+      div(class: 'content') do
+        h1 do
+          text 'Next '
+          span('Sounders', class: 'sounders')
+          text ' Game'
+        end
+
+        h3(class: 'countdown')
+
+        div(class: 'text-content') do
+          p(class: 'game-content')
+        end
+
+        div(class: 'text-content') do
+          h3(class: 'home-game-header')
+          p(class: 'home-game-content')
+        end
+
+        div(class: 'text-content') do
+          p(class: 'suggestion') do
+            text 'In the meantime '
+            span(class: 'suggestion-links') do
+              a('get smart', href: 'https://www.amazon.com/Inverting-Pyramid-History-Soccer-Tactics/dp/1568587384')
+              text ' or '
+              a('get hyped', href: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=seattle+sounders')
+              text '.'
+            end
+          end
+        end
+      end
     end
   end
 
@@ -30,8 +61,13 @@ class Index < Fortitude::Widget
 
       title('Sounders FC Game countdown!')
 
+      css_tag('main.css')
       js_tag('app.js')
     end
+  end
+
+  def css_tag(src)
+    link(rel: 'stylesheet', href: src)
   end
 
   def js_tag(src)
