92628140134bcc5693e8f1c846d14c2300b73315
bion <bionjohnson@gmail.com>
2019-02-28 20:11:11 -0800

    2019

--- app.js
+++ app.js
@@ -2,220 +2,238 @@ window.NSG = {};
 
 NSG.GAME_DATA = [
   {
-    "date": "Thursday, February 22, 2018 7:00PM PST",
-    "matchup": "SANTA TECLA FC",
-    "location": "ESTADIO LAS DELICIAS",
-    "time_in_milliseconds": 1519351200000
+    "date": "Saturday, February 9, 2019 5:00PM PST",
+    "matchup": "HOUSTON DYNAMO",
+    "location": "KINO SPORTS COMPLEX",
+    "time_in_milliseconds": 1549756800000
   },
   {
-    "date": "Thursday, March 1, 2018 7:00PM PST",
-    "matchup": "SANTA TECLA FC",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1519956000000
+    "date": "Wednesday, February 13, 2019 6:00PM PST",
+    "matchup": "PORTLAND TIMBERS",
+    "location": "KINO SPORTS COMPLEX",
+    "time_in_milliseconds": 1550106000000
   },
   {
-    "date": "Sunday, March 4, 2018 2:00PM PST",
-    "matchup": "LOS ANGELES FOOTBALL CLUB",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1520197200000
-  },
-  {
-    "date": "Sunday, March 18, 2018 2:00PM PST",
+    "date": "Saturday, February 16, 2019 9:00AM PST",
     "matchup": "FC DALLAS",
-    "location": "TOYOTA STADIUM",
-    "time_in_milliseconds": 1521406800000
+    "location": "KINO SPORTS COMPLEX",
+    "time_in_milliseconds": 1550332800000
   },
   {
-    "date": "Saturday, March 31, 2018 7:00PM PST",
-    "matchup": "MONTREAL IMPACT",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1522548000000
+    "date": "Wednesday, February 20, 2019 7:30PM PST",
+    "matchup": "CLUB NACIONAL DE FOOTBALL",
+    "location": "CENTURYLINK FIELD, SEATTLE, WA",
+    "time_in_milliseconds": 1550716200000
   },
   {
-    "date": "Sunday, April 15, 2018 1:00PM PST",
-    "matchup": "SPORTING KANSAS CITY",
-    "location": "SPORTING PARK",
-    "time_in_milliseconds": 1523822400000
-  },
-  {
-    "date": "Sunday, April 22, 2018 1:00PM PST",
-    "matchup": "MINNESOTA UNITED FC",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1524427200000
-  },
-  {
-    "date": "Sunday, April 29, 2018 6:00PM PST",
-    "matchup": "LOS ANGELES FOOTBALL CLUB",
-    "location": "BANC OF CALIFORNIA STADIUM",
-    "time_in_milliseconds": 1525050000000
-  },
-  {
-    "date": "Saturday, May 5, 2018 1:00PM PST",
-    "matchup": "COLUMBUS CREW SC",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1525550400000
-  },
-  {
-    "date": "Wednesday, May 9, 2018 4:30PM PST",
-    "matchup": "TORONTO FC",
-    "location": "BMO FIELD",
-    "time_in_milliseconds": 1525908600000
-  },
-  {
-    "date": "Sunday, May 13, 2018 1:00PM PST",
-    "matchup": "PORTLAND TIMBERS",
-    "location": "PROVIDENCE PARK",
-    "time_in_milliseconds": 1526241600000
-  },
-  {
-    "date": "Saturday, May 26, 2018 1:00PM PST",
-    "matchup": "REAL SALT LAKE",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1527364800000
-  },
-  {
-    "date": "Saturday, June 2, 2018 6:30PM PST",
-    "matchup": "REAL SALT LAKE",
-    "location": "RIO TINTO STADIUM",
-    "time_in_milliseconds": 1527989400000
-  },
-  {
-    "date": "Saturday, June 9, 2018 7:00PM PST",
-    "matchup": "D.C. UNITED",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1528596000000
-  },
-  {
-    "date": "Wednesday, June 13, 2018 5:00PM PST",
-    "matchup": "NEW YORK RED BULLS",
-    "location": "RED BULL ARENA (USA)",
-    "time_in_milliseconds": 1528934400000
-  },
-  {
-    "date": "Saturday, June 23, 2018 7:00PM PST",
-    "matchup": "CHICAGO FIRE",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1529805600000
-  },
-  {
-    "date": "Saturday, June 30, 2018 1:30PM PST",
-    "matchup": "PORTLAND TIMBERS",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1530390600000
-  },
-  {
-    "date": "Wednesday, July 4, 2018 6:00PM PST",
-    "matchup": "COLORADO RAPIDS",
-    "location": "DICK'S SPORTING GOODS PARK",
-    "time_in_milliseconds": 1530752400000
-  },
-  {
-    "date": "Saturday, July 7, 2018 4:30PM PST",
-    "matchup": "NEW ENGLAND REVOLUTION",
-    "location": "GILLETTE STADIUM",
-    "time_in_milliseconds": 1531006200000
-  },
-  {
-    "date": "Sunday, July 15, 2018 11:00AM PST",
-    "matchup": "ATLANTA UNITED FC",
-    "location": "MERCEDES-BENZ STADIUM",
-    "time_in_milliseconds": 1531677600000
-  },
-  {
-    "date": "Saturday, July 21, 2018 1:00PM PST",
-    "matchup": "VANCOUVER WHITECAPS FC",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1532203200000
-  },
-  {
-    "date": "Wednesday, July 25, 2018 7:30PM PST",
+    "date": "Saturday, February 23, 2019 12:45PM PST",
     "matchup": "SAN JOSE EARTHQUAKES",
     "location": "AVAYA STADIUM",
-    "time_in_milliseconds": 1532572200000
+    "time_in_milliseconds": 1550951100000
   },
   {
-    "date": "Sunday, July 29, 2018 2:00PM PST",
-    "matchup": "NEW YORK CITY FC",
+    "date": "Saturday, March 2, 2019 7:00PM PST",
+    "matchup": "FC CINCINNATI",
     "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1532898000000
+    "time_in_milliseconds": 1551578400000
   },
   {
-    "date": "Saturday, August 4, 2018 5:00PM PST",
-    "matchup": "MINNESOTA UNITED FC",
-    "location": "TCF BANK STADIUM",
-    "time_in_milliseconds": 1533427200000
-  },
-  {
-    "date": "Sunday, August 12, 2018 7:00PM PST",
-    "matchup": "FC DALLAS",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1534125600000
-  },
-  {
-    "date": "Saturday, August 18, 2018 12:30PM PST",
-    "matchup": "LA GALAXY",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1534620600000
-  },
-  {
-    "date": "Sunday, August 26, 2018 6:30PM PST",
-    "matchup": "PORTLAND TIMBERS",
-    "location": "PROVIDENCE PARK",
-    "time_in_milliseconds": 1535333400000
-  },
-  {
-    "date": "Saturday, September 1, 2018 1:00PM PST",
-    "matchup": "SPORTING KANSAS CITY",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1535832000000
-  },
-  {
-    "date": "Saturday, September 15, 2018 7:00PM PST",
-    "matchup": "VANCOUVER WHITECAPS FC",
-    "location": "BC PLACE",
-    "time_in_milliseconds": 1537063200000
-  },
-  {
-    "date": "Wednesday, September 19, 2018 7:30PM PST",
-    "matchup": "PHILADELPHIA UNION",
-    "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1537410600000
-  },
-  {
-    "date": "Sunday, September 23, 2018 4:00PM PST",
-    "matchup": "LA GALAXY",
-    "location": "STUBHUB CENTER",
-    "time_in_milliseconds": 1537743600000
-  },
-  {
-    "date": "Saturday, September 29, 2018 1:00PM PST",
+    "date": "Saturday, March 9, 2019 7:00PM PST",
     "matchup": "COLORADO RAPIDS",
     "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1538251200000
+    "time_in_milliseconds": 1552183200000
   },
   {
-    "date": "Saturday, October 6, 2018 TBD",
-    "matchup": "HOUSTON DYNAMO",
+    "date": "Saturday, March 16, 2019 10:00AM PST",
+    "matchup": "CHICAGO FIRE",
+    "location": "SEATGEEK STADIUM",
+    "time_in_milliseconds": 1552755600000
+  },
+  {
+    "date": "Saturday, March 30, 2019 7:00PM PST",
+    "matchup": "VANCOUVER WHITECAPS FC",
+    "location": "BC PLACE",
+    "time_in_milliseconds": 1553997600000
+  },
+  {
+    "date": "Saturday, April 6, 2019 7:00PM PST",
+    "matchup": "REAL SALT LAKE",
     "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1538809200000
+    "time_in_milliseconds": 1554602400000
   },
   {
-    "date": "Wednesday, October 17, 2018 4:30PM PST",
-    "matchup": "ORLANDO CITY SC",
-    "location": "ORLANDO CITY STADIUM",
-    "time_in_milliseconds": 1539819000000
+    "date": "Wednesday, April 10, 2019 6:00PM PST",
+    "matchup": "COLORADO RAPIDS",
+    "location": "DICK'S SPORTING GOODS PARK",
+    "time_in_milliseconds": 1554944400000
   },
   {
-    "date": "Sunday, October 21, 2018 TBD",
-    "matchup": "HOUSTON DYNAMO",
-    "location": "BBVA COMPASS STADIUM",
-    "time_in_milliseconds": 1540105200000
+    "date": "Saturday, April 13, 2019 1:00PM PST",
+    "matchup": "TORONTO FC",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1555185600000
   },
   {
-    "date": "Sunday, October 28, 2018 1:30PM PST",
+    "date": "Sunday, April 21, 2019 4:00PM PST",
+    "matchup": "LOS ANGELES FOOTBALL CLUB",
+    "location": "BANC OF CALIFORNIA STADIUM",
+    "time_in_milliseconds": 1555887600000
+  },
+  {
+    "date": "Wednesday, April 24, 2019 7:30PM PST",
     "matchup": "SAN JOSE EARTHQUAKES",
     "location": "CENTURYLINK FIELD",
-    "time_in_milliseconds": 1540758600000
+    "time_in_milliseconds": 1556159400000
+  },
+  {
+    "date": "Sunday, April 28, 2019 12:30PM PST",
+    "matchup": "LOS ANGELES FOOTBALL CLUB",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1556479800000
+  },
+  {
+    "date": "Saturday, May 4, 2019 5:00PM PST",
+    "matchup": "MINNESOTA UNITED FC",
+    "location": "ALLIANZ FIELD",
+    "time_in_milliseconds": 1557014400000
+  },
+  {
+    "date": "Saturday, May 11, 2019 7:00PM PST",
+    "matchup": "HOUSTON DYNAMO",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1557626400000
+  },
+  {
+    "date": "Wednesday, May 15, 2019 7:30PM PST",
+    "matchup": "ORLANDO CITY SC",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1557973800000
+  },
+  {
+    "date": "Saturday, May 18, 2019 4:30PM PST",
+    "matchup": "PHILADELPHIA UNION",
+    "location": "TALEN ENERGY STADIUM",
+    "time_in_milliseconds": 1558222200000
+  },
+  {
+    "date": "Sunday, May 26, 2019 3:00PM PST",
+    "matchup": "SPORTING KANSAS CITY",
+    "location": "CHILDREN'S MERCY PARK",
+    "time_in_milliseconds": 1558908000000
+  },
+  {
+    "date": "Saturday, June 1, 2019 5:00PM PST",
+    "matchup": "FC DALLAS",
+    "location": "TOYOTA STADIUM",
+    "time_in_milliseconds": 1559433600000
+  },
+  {
+    "date": "Wednesday, June 5, 2019 4:30PM PST",
+    "matchup": "MONTREAL IMPACT",
+    "location": "STADE SAPUTO",
+    "time_in_milliseconds": 1559777400000
+  },
+  {
+    "date": "Saturday, June 29, 2019 7:00PM PST",
+    "matchup": "VANCOUVER WHITECAPS FC",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1561860000000
+  },
+  {
+    "date": "Wednesday, July 3, 2019 4:00PM PST",
+    "matchup": "NEW YORK CITY FC",
+    "location": "YANKEE STADIUM",
+    "time_in_milliseconds": 1562194800000
+  },
+  {
+    "date": "Saturday, July 6, 2019 4:30PM PST",
+    "matchup": "COLUMBUS CREW SC",
+    "location": "MAPFRE STADIUM",
+    "time_in_milliseconds": 1562455800000
+  },
+  {
+    "date": "Sunday, July 14, 2019 1:00PM PST",
+    "matchup": "ATLANTA UNITED FC",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1563134400000
+  },
+  {
+    "date": "Sunday, July 21, 2019 6:30PM PST",
+    "matchup": "PORTLAND TIMBERS",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1563759000000
+  },
+  {
+    "date": "Saturday, July 27, 2019 5:00PM PST",
+    "matchup": "HOUSTON DYNAMO",
+    "location": "BBVA COMPASS STADIUM",
+    "time_in_milliseconds": 1564272000000
+  },
+  {
+    "date": "Sunday, August 4, 2019 7:00PM PST",
+    "matchup": "SPORTING KANSAS CITY",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1564970400000
+  },
+  {
+    "date": "Saturday, August 10, 2019 1:00PM PST",
+    "matchup": "NEW ENGLAND REVOLUTION",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1565467200000
+  },
+  {
+    "date": "Wednesday, August 14, 2019 7:00PM PST",
+    "matchup": "REAL SALT LAKE",
+    "location": "RIO TINTO STADIUM",
+    "time_in_milliseconds": 1565834400000
+  },
+  {
+    "date": "Saturday, August 17, 2019 7:00PM PST",
+    "matchup": "LA GALAXY",
+    "location": "DIGNITY HEALTH SPORTS PARK",
+    "time_in_milliseconds": 1566093600000
+  },
+  {
+    "date": "Friday, August 23, 2019 7:00PM PST",
+    "matchup": "PORTLAND TIMBERS",
+    "location": "PROVIDENCE PARK",
+    "time_in_milliseconds": 1566612000000
+  },
+  {
+    "date": "Sunday, September 1, 2019 3:00PM PST",
+    "matchup": "LA GALAXY",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1567375200000
+  },
+  {
+    "date": "Sunday, September 15, 2019 12:30PM PST",
+    "matchup": "NEW YORK RED BULLS",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1568575800000
+  },
+  {
+    "date": "Wednesday, September 18, 2019 7:30PM PST",
+    "matchup": "FC DALLAS",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1568860200000
+  },
+  {
+    "date": "Sunday, September 22, 2019 5:00PM PST",
+    "matchup": "D.C. UNITED",
+    "location": "AUDI FIELD",
+    "time_in_milliseconds": 1569196800000
+  },
+  {
+    "date": "Sunday, September 29, 2019 4:30PM PST",
+    "matchup": "SAN JOSE EARTHQUAKES",
+    "location": "AVAYA STADIUM",
+    "time_in_milliseconds": 1569799800000
+  },
+  {
+    "date": "Sunday, October 6, 2019 1:00PM PST",
+    "matchup": "MINNESOTA UNITED FC",
+    "location": "CENTURYLINK FIELD",
+    "time_in_milliseconds": 1570392000000
   }
 ];
 
